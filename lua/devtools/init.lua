0f92e8dc568cc51ef13e3eef753f9e2a0678feda
Neiklneev <neikln33v@gmail.com>
2024-10-17 14:26:54 +0400

    added run_file function

--- lua/devtools/init.lua
+++ lua/devtools/init.lua
@@ -1,16 +1,15 @@
 local function setup()
-  vim.api.nvim_create_user_command('RunPython', function()
-    local filename = vim.fn.expand('%:p')
-
-    vim.cmd('split | terminal')
-
-    vim.api.nvim_feedkeys('python3 ' .. filename .. '\n', 't', false)
-  end, {})
+  vim.api.nvim_create_user_command('RunPython', run_file, {})
 end
 
 local function map()
   vim.api.nvim_set_keymap('n', "<leader>r", ':RunPython<CR>', { noremap = true, silent = true })
 end
 
+local function run_file()
+  local filename = vim.fn.expand('%:p')
+    vim.cmd('split | terminal')
+    vim.api.nvim_feedkeys('python3 ' .. filename .. '\n', 't', false)
+end
 
 
